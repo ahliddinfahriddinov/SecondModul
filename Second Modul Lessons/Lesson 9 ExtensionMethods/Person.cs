2a4e428aa1d2d734cb92c3b9b92bc8152de41341
Ahliddin Fahriddinov <fahriddinovahliddin687@gmail.com>
2025-01-04 12:56:02 +0500

    code add

--- Second Modul Lessons/Lesson 9 ExtensionMethods/Person.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lesson_9_ExtensionMethods;
-
-public class Person
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public int Age { get; set; }
-}
